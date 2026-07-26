67bc8e641ad0bb2722f63f5e0d3ab6c93293a5fd
s12kuma01 <s12kuma01@tuta.com>
2026-07-27 08:07:49 +0900

    update readme

--- src/main/java/com/sumirelabs/pulsar/Pulsar.java
+++ src/main/java/com/sumirelabs/pulsar/Pulsar.java
@@ -1,9 +1,13 @@
 package com.sumirelabs.pulsar;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import com.sumirelabs.pulsar.command.CommandPulsar;
 import com.sumirelabs.pulsar.config.PulsarConfig;
 import com.sumirelabs.pulsar.light.engine.FaceOcclusion;
 import com.sumirelabs.pulsar.proxy.IProxy;
+
 import net.minecraftforge.common.config.ConfigManager;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.SidedProxy;
@@ -11,8 +15,6 @@ import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLServerStartingEvent;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 
 @Mod(modid = Reference.MOD_ID, name = Reference.MOD_NAME, version = Reference.VERSION, acceptableRemoteVersions = "*")
 public class Pulsar {
@@ -48,7 +50,7 @@ public class Pulsar {
     public void postInit(FMLPostInitializationEvent event) {
         proxy.postInit(event);
         FaceOcclusion.registerDefaults();
-        LOGGER.info("{} ready - scalar BFS lighting engine active.", Reference.MOD_NAME);
+        LOGGER.info("{} ready - Pulsar Lighting Engine active.", Reference.MOD_NAME);
     }
 
     @Mod.EventHandler
